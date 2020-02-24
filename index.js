269c601cf6c06cbadf1ba62e6a892990ca72f8bd
Socheat Chhun <chhunsocheat@gmail.com>
2020-02-24 22:25:19 +1100

    sign in form

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRepo = require('./user')
-const cookieSession= require('coockie-session')
+const cookieSession= require('cookie-session')
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
   keys:['asdasddfbrghherbdf']
 }))
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
+     Your Id  ${req.session.userId}
       <form method="POST">
         <input name="email" placeholder="email" />
         <input name="password" placeholder="password" />
@@ -21,7 +22,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
 const{email,password,passwordConfirmation}=req.body; 
 const existingUser=await userRepo.getOneBy({email})
 if(existingUser){
@@ -40,6 +41,23 @@ res.send("Account created")
 
   
 });
+
+app.get('/signout',(req,res)=>{
+  req.session=null;
+  req.send("You are logged out")
+})
+
+app.get("/signin",(req,res)=>{
+  res.send(`
+  <div>
+   <form method="POST">
+     <input name="email" placeholder="email" />
+     <input name="password" placeholder="password" />
+     <button>Sign In</button>
+   </form>
+ </div>
+  `)
+})
 
 app.listen(3000, () => {
   console.log('Listening');
