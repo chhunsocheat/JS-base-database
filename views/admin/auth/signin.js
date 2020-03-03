37f75d03a8eec5b986719dd4c2f73c26a2e26285
Socheat Chhun <chhunsocheat@gmail.com>
2020-03-03 18:52:32 +1100

    new style

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,6 +1,7 @@
-const layout = require ("../layout")
+const layout = require('../layout');
+//const { getError } = require('../../helpers');
 
-const getError=(errors,props)=>{
+getError=(errors,props)=>{
   try{
 
     return errors.mapped()[props].msg;
@@ -10,17 +11,30 @@ const getError=(errors,props)=>{
   }
 }
 
-module.exports=({errors})=>{
-    return layout({
-    content:`<div>
-      <form method="POST">
-      <input name="email" placeholder="email" />
-      ${getError(error,"email")}
-        <input name="password" placeholder="password" />
-        ${getError(error,"password")}
-
-        <button>Sign In</button>
-      </form>
-    </div>
-  `})
-}
+module.exports = ({ errors }) => {
+  return layout({
+    content: `
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-one-quarter">
+            <form method="POST">
+              <h1 class="title">Sign in</h1>
+              <div class="field">
+                <label class="label">Email</label>
+                <input required class="input" placeholder="Email" name="email" />
+                <p class="help is-danger">${getError(errors, 'email')}</p>
+              </div>
+              <div class="field">
+                <label class="label">Password</label>
+                <input required class="input" placeholder="Password" name="password" type="password" />
+                <p class="help is-danger">${getError(errors, 'password')}</p>
+              </div>
+              <button class="button is-primary">Submit</button>
+            </form>
+            <a href="/signup">Need an account? Sign Up</a>
+          </div>
+        </div>
+      </div>
+    `
+  });
+};
---
+++ views/helpers.js
@@ -0,0 +1,14 @@
+module.exports={
+    getError=(errors,props)=>{
+        try{
+      
+          return errors.mapped()[props].msg;
+          //return errors object
+        }catch(err){
+          return " ";
+        }
+      }
+      
+      
+    
+}
