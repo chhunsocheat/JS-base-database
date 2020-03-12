c377f64360a63b6b5a0ce009b4c300ad360ea6c6
Socheat Chhun <chhunsocheat@gmail.com>
2020-03-12 23:20:58 +1100

    new input

--- views/admin/product/new.js
+++ views/admin/product/new.js
@@ -7,7 +7,9 @@ module.exports=({errors})=>{
         `
         <form method="POST" enctype="multipart/form-data">
         <input placeholder="Title" name="title" />
-        <input placeholder="Price" name="price" />
+        <input placeholder="Price" name="price" /> 
+
+        <input placeholder="Price" name="price" /> 
         <input type="file" name="image" />
         <button>Submit</button>
         </form>
