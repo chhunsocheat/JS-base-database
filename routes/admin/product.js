310b2e05f99cb98f3ae27cf11cc7064d0c8f3ee3
Socheat Chhun <chhunsocheat@gmail.com>
2020-03-25 22:50:29 +1100

    product form fixed

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -1,8 +1,8 @@
 const express=require('express')
 const{validationResult}=require('express-validator')
 const multer=require("multer");
-
-
+const {handleErrors}=require("./middleware")
+const productTemplate=require("../../views/admin/product/index")
 const productsRepo=require('../../repositories/product')
 const productsNewTemplate=require("../../views/admin/product/new")
 const {requireTitle,requirePrice}=require("./validator")
@@ -10,27 +10,32 @@ const router=express.Router();
 const upload=multer({
     storage:multer.memoryStorage()
 })
-router.get("/admin/products",(req,res)=>{
-
-
+router.get("/admin/products",async (req,res)=>{
+const products = await productsRepo.getAll();
+res.send(productTemplate({products})); 
 })
 router.get("/admin/products/new",(req,res)=>{
 res.send(productsNewTemplate({}))
 
 })
-router.post("/admin/products/new",[requireTitle,requirePrice],upload.single('image'),async (req,res)=>{
-   const errors=validationResult(req);
+router.post("/admin/products/new"
+,upload.single('image')
+,[requireTitle,requirePrice]
+,handleErrors(productsNewTemplate)
+,async (req,res)=>{
+ 
 
-   if(!errors.isEmpty()){
-       return res.send(productsNewTemplate({errors}));
-   }
-   const image=req.file.buffer.toString("base64");
-   const {title,price}=req.body;
-   await productsRepo.create({
-    title,price,image
-   })
-    res.send("Success")
-
+      
+     const image=req.file.buffer.toString("base64");
+     if(req.file.buffer===undefined){
+         return res.send(productsNewTemplate({}))
+     }
+     const {title,price}=req.body;
+     await productsRepo.create({
+      title,price,image
+     })
+      res.send("Success")
+  
 })
 // router.get("/admin/product",(req,res)=>{
 
